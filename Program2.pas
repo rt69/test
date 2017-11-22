f15e3f2db32353b3b42d8f769f9f485f6738c0c8
Your Name <you@example.com>
2017-11-22 15:21:30 +0300

    pascal sample

---
+++ Program2.pas
@@ -0,0 +1,15 @@
+program awesomeprogram;
+
+var i,h:integer;
+
+begin
+  write('write h: ');
+  readln(h);
+
+  //cycle from 1 to h
+  for i:= 1 to h do //
+  begin
+    write('hello world ');
+    writeln(i);
+  end;
+end.
